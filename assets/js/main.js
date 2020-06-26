a30b19045b5e6e23e8ee042aa30a430192402ae0
Mike S Chen <mike.sihong.chen@gmail.com>
2020-06-26 08:46:35 -0400

    implemented local storage get and set

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,19 +9,18 @@ var tasks = [];
 $(document).ready(function () {
   displayToday();
   displayTimeBlocks();
+  displayTasks();
 
-  
   $(".saveBtn").on("click", function (event) {
     event.preventDefault();
-    console.log(event.target.value);
-    console.log("working");
-    console.log($(".hour").val());
-    // saveTask();
+    let timeValue = $(event.currentTarget).siblings(":first-child").attr("value");
+    let textEntry = $(event.currentTarget).siblings(":nth-child(2)").val();
+    saveTask(timeValue, textEntry);
   });
 });
 
 function displayToday() {
-  var today = moment().format("[Today is] dddd LL");
+  let today = moment().format("[Today is] dddd LL");
   $("#currentDay").text(today);
 }
 
@@ -29,13 +28,13 @@ function displayTimeBlocks() {
   let blockTime = moment("06:00", "HH:mm", true);
   for (let i = 0; i < numberOfTimeBlocks; i++) {
     let timeblock = $('<form class="row time-block"></form>');
-    let timeEl = $('<div class="col-2 hour"></div>');
+    let timeEl = $('<label class="col-2 hour"></label>');
     let textareaEl = $('<textarea class="col"></textarea>');
     let buttonEl = $('<button type="submit" class="col-2 saveBtn"></button>');
     timeblock.append(timeEl, textareaEl, buttonEl);
 
     $(".container").append(timeblock);
-    timeEl.attr("id", i);
+    timeEl.attr("value", blockTime.format("LT"));
     timeEl.text(blockTime.format("LT"));
     buttonEl.text("save");
     buttonEl.attr("value", i);
@@ -51,9 +50,25 @@ function displayTimeBlocks() {
   }
 }
 
-function saveTask() {
-  let hour = $(".id");
-  console.log(hour);
-  let task = { time: "", description: "" };
-  
+function displayTasks() {
+  tasks = JSON.parse(localStorage.getItem("tasks"));
+  if (tasks !== null) {
+    for (let i = 0; i < tasks.length; i++) {
+      $('label[value="' + tasks[i].time + '"]').next().text(tasks[i].description);
+    }
+  }
+}
+
+function saveTask(hour, text) {
+  let task = { time: hour, description: text };
+  if (localStorage.getItem("tasks") === null) {
+    // if localStorage is empty, setItem first
+    tasks.push(task);
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  } else {
+    // if localStorage is not empty, getItem first
+    tasks = JSON.parse(localStorage.getItem("tasks"));
+    tasks.push(task);
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }
 }
