cd3beccefffb7b8d95079357f870ca7e2aa989f3
Mike S Chen <mike.sihong.chen@gmail.com>
2020-06-26 07:08:10 -0400

    color coded timeblocks and completed basic layout

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,34 +1,59 @@
 moment().format();
 
-var numberOfTimeBlocks = 8;
-var timeIncrement = 1;
+var numberOfTimeBlocks = 12;
+
 var currentTime = moment();
+
+var tasks = [];
 
 $(document).ready(function () {
   displayToday();
   displayTimeBlocks();
-  console.log(currentTime);
+
+  
+  $(".saveBtn").on("click", function (event) {
+    event.preventDefault();
+    console.log(event.target.value);
+    console.log("working");
+    console.log($(".hour").val());
+    // saveTask();
+  });
 });
 
-
-
 function displayToday() {
-  var today = moment().format("[Today is] LL");
-  $("#today").css("color", "blue");
-  $("#today").text(today);
+  var today = moment().format("[Today is] dddd LL");
+  $("#currentDay").text(today);
 }
 
 function displayTimeBlocks() {
-  for (let i = 0; i < numberOfTimeBlocks; i++){
-    let timeblock = $('<div class="row"></div>');
-    let time = $('<div class="col-2 time"></div>');
-    let task = $('<div class="col task"></div>');
-    let button = $('<div class="col-2 button"></div>');
+  let blockTime = moment("06:00", "HH:mm", true);
+  for (let i = 0; i < numberOfTimeBlocks; i++) {
+    let timeblock = $('<form class="row time-block"></form>');
+    let timeEl = $('<div class="col-2 hour"></div>');
+    let textareaEl = $('<textarea class="col"></textarea>');
+    let buttonEl = $('<button type="submit" class="col-2 saveBtn"></button>');
+    timeblock.append(timeEl, textareaEl, buttonEl);
+
     $(".container").append(timeblock);
-    timeblock.append(time, task, button);
-    time.text(i);
-    task.text("test");
-    button.text("button");
+    timeEl.attr("id", i);
+    timeEl.text(blockTime.format("LT"));
+    buttonEl.text("save");
+    buttonEl.attr("value", i);
+
+    if (currentTime.diff(blockTime) > 3599999) {
+      textareaEl.addClass("past");
+    } else if (currentTime.diff(blockTime) < 0) {
+      textareaEl.addClass("future");
+    } else {
+      textareaEl.addClass("present");
+    }
+    blockTime.add(1, "hour");
   }
 }
 
+function saveTask() {
+  let hour = $(".id");
+  console.log(hour);
+  let task = { time: "", description: "" };
+  
+}
